63cd52c5a6e23582295f5500e827f8533354972e
Romana Wilschut <romana400@gmail.com>
2021-04-29 12:14:09 +0200

    split data creation and  main into two files

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ INFO:
 """
 
 from setup import SmartVoting, create_ballot
-from data import create_data
+from create_data import create_data
 
 if __name__ == "__main__":
     # State parameters of smart ballot
-    preference_level = 3
+    preference_level = 4
     num_agents = 5
     delegation_bound_lower = 0
     delegation_bound_upper = 1
@@ -19,11 +19,14 @@ if __name__ == "__main__":
 
     for i in range(1, amount_ballots + 1):
         file = f"{i}_{num_agents}_{preference_level}_{delegation_bound_lower}_{delegation_bound_upper}.csv"
-        create_data(file, num_agents, preference_level, [delegation_bound_lower, delegation_bound_upper])
 
-        # Create dataframe 
+        # Create dataframe of datafile
+        # Change path to where your data is
         ballot, agents = create_ballot(f"data/{file}", num_agents, preference_level)
-        print(ballot)
 
+        # Create smart voting model
         Voting = SmartVoting(ballot, agents)
-        Voting.calculate_output()
+        # Unravel the ballot
+        outcome = Voting.unravel()
+
+        print(outcome, "\n")
