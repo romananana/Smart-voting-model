713282e489354a12d0bf87228f0edb13c9bb2346
Romana Wilschut <romana400@gmail.com>
2021-07-06 14:59:21 +0200

    added info; added explantions

--- cycles.py
+++ cycles.py
@@ -1,12 +1,24 @@
-import networkx as nx
-import matplotlib.pyplot as plt
+"""
+DISCLAIMER: This file was created for the thesis of Romana Wilschut for the 
+            bachelor ‘Artificial Intelligence’ at the University of Amsterdam.
+NAME:       Romana Wilschut 
+UVA ID:     12156884
+INFO:       This file consists of a function that finds all cycles in a 
+            preference level of a valid smart profile.
+"""
 
-def find_cycles(Voting, Y, preference_level):
+import networkx as nx
+
+def find_cycles(Voting, preference_level):
+    """
+    Finds all cycles that occur in a preference level.
+    returns: all found cycles
+    """
     linked = {agent : [] for agent in Voting.agents}
 
     for i, line in enumerate(Voting.profile[preference_level]):
         for agent in Voting.agents:
-            if agent in line and Y[Voting.agents[i]] is None:
+            if agent in line and Voting.X[Voting.agents[i]] is None:
                 linked[Voting.agents[i]].append(agent)
 
     g = nx.DiGraph()
@@ -18,14 +30,4 @@ def find_cycles(Voting, Y, preference_level):
     cycles = nx.simple_cycles(g)
     
     return cycles    
-
-def cycle_duplicates(cycles, target_cycle):
-    first_letter = sorted(target_cycle)[0]
-    index = target_cycle.index(first_letter)
-    target_cycle = target_cycle[index:] + target_cycle[:index]
-
-    if tuple(target_cycle) in cycles:
-        return True
-
-    return False
         
